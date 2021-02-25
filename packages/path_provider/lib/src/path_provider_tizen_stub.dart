b40f308e1aa6b96e1cc0d40ebd146b2566a62ce7
Swift Kim <swift.kim@samsung.com>
2021-02-25 15:54:19 +0900

    Add stubs

---
+++ packages/path_provider/lib/src/path_provider_tizen_stub.dart
@@ -0,0 +1,9 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+
+class PathProviderPlugin extends PathProviderPlatform {
+  PathProviderPlugin() : assert(false);
+}
